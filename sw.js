70e7b2db870cd20816c66195be79e55e778a58d7
bbyyttrrryyyyy-design <bbyyttrrryyyyy@gmail.com>
2026-09-20 17:53:30 +0800

    修正离线缓存路径

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE='family-birthday-v1';const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg','/lunar.csv'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='family-birthday-v2';const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','lunar.csv'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+
